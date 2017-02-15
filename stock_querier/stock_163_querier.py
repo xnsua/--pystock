d69ee57cdfa485a10a64271e43c9fc0fd078466f
xnsua <cqhme@outlook.com>
2017-02-15 11:57:01 +0800

    Mod : Tiny

--- stock_querier/stock_163_querier.py
+++ stock_querier/stock_163_querier.py
@@ -2,6 +2,7 @@ from utilities.import_basic import *
 
 def query_stock_history(stockcode, starttime, endtime):
 #     http://quotes.money.163.com/service/chddata.html?code=0000001&start=19901219&end=20170214&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;VOTURNOVER;VATURNOVER
+    pass
 
 
 

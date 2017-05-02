cdb645c6a02df5d05438251fe49f3920477268c6
xnsua <cqhme@outlook.com>
2017-05-02 19:18:44 +0800

    Mod: tidy

--- common/cache_util.py
+++ common/cache_util.py
@@ -8,9 +8,9 @@ def cache(cache_time_delta):
         def func_wrapper(*args, **kwargs):
             cache_key = (func.__name__, args, frozenset(kwargs.items()))
             if cache_key in _memory_cache:
-                cache_time, ache_val = _memory_cache[cache_key]
+                cache_time, cache_val = _memory_cache[cache_key]
                 if dt.datetime.now() - cache_time < cache_time_delta:
-                    return _memory_cache[cache_key][1]
+                    return cache_val
 
             rval = func(*args, **kwargs)
             _memory_cache[cache_key] = (dt.datetime.now(), rval)
--- jqs.py
+++
@@ -1,4 +0,0 @@
-import tushare
-
-val = tushare.get_k_data('510900')
-print(val)
